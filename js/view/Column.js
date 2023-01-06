380092e2c16aec9ec5321a2878aa26e0c2d85d8d
Natalie <nataliejhong@gmail.com>
2023-01-06 17:49:21 -0600

    added kanban, column, and item view

---
+++ js/view/Column.js
@@ -0,0 +1,40 @@
+import kanbanAPI from "../api/kanbanAPI"
+
+export default class Column{
+    constructor(id, title){
+        this.elements = {}
+        this.elements.root = Column.createRoot()
+        this.elements.title = this.elements.root.querySelector(".kanban_column-title")
+        this.elements.items = this.elements.root.querySelector(".kanban_column-items")
+        this.elements.addItem = this.elements.root.querySelector(".kanban_add-item")
+
+        this.elements.root.dataset.id = id
+        this.elements.title.textContent = title
+
+        this.elements.addItem.addEventListener("click",
+        () => {
+            const newItem = kanbanAPI.insertItem(id, "")
+            this.renderItem(newItem)
+        })
+    }
+
+    static createRoot(){
+        const range = document.createRange()
+
+        range.selectNode(document.body)
+
+        return range.createContextualFragment(`
+            <div class="kanban_column">
+                <div class="kanban_column-title"></div>
+                <div class="kanban_column-items"></div>
+                <div class="kanban_column-item"></div>
+                <button class="kanban_add-item" type="button">+Add</button>
+            </div>
+        `).children[0]
+    }
+
+    renderItem(data){
+        const item = new Item(data.id, data.content)
+        this.elements.items.appendChild(item.elements.root)
+    }
+}
---
+++ js/view/Kanban.js
@@ -0,0 +1,30 @@
+export default class Kanban {
+    constructor(root){
+        this.root = root
+
+        Kanban.columns().forEach(column => {
+            //TODO: Create instance of column class
+        })
+    }
+
+    static columns(){
+        return [
+            {
+                id: 1,
+                title: "Not Started"
+            },
+            {
+                id: 2,
+                title: "In Progress"
+            },
+            {
+                id: 3,
+                title: "Completed"
+            },
+            {
+                id: 4,
+                title: "On Hold"
+            },
+        ]
+    }
+}
